10953ba74e10fd046fec864d975e76b988b76161
Anar Aydinli <109851305+anaraydinli55@users.noreply.github.com>
2026-06-19 14:26:01 +0400

    Create testimonials.js

---
+++ src/data/testimonials.js
@@ -0,0 +1,27 @@
+// Using keys from translation file for easier management
+export const testimonials = [
+  {
+    id: 1,
+    customerNameKey: 'testimonials.customer_name_1',
+    reviewKey: 'testimonials.customer_review_1',
+    rating: 5,
+  },
+  {
+    id: 2,
+    customerNameKey: 'testimonials.customer_name_2',
+    reviewKey: 'testimonials.customer_review_2',
+    rating: 5,
+  },
+  {
+    id: 3,
+    customerNameKey: 'testimonials.customer_name_3',
+    reviewKey: 'testimonials.customer_review_3',
+    rating: 4,
+  },
+  {
+    id: 4,
+    customerNameKey: 'testimonials.customer_name_1', // Reusing for variety
+    reviewKey: 'testimonials.customer_review_1',
+    rating: 5,
+  }
+];
